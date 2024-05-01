e0ae68fe4ae00548521d6721d3adc9451494eae9
Mathieu Magnin <mathieu.magnin@gmail.com>
2024-05-01 09:58:09 +0200

    style

--- app/caisse/page.tsx
+++ app/caisse/page.tsx
@@ -26,41 +26,43 @@ export default function Caisse() {
     <>
       <h1>Caisse</h1>
 
-      {items.map((item, index) => (
-        <div key={item.name}>
-          <div className="flex gap-x-4">
-            <div className="flex-1">{item.name}</div>
-            <div className="flex-1">{item.price}</div>
+      <div className="flex flex-col divide-y divide-gray-500">
+        {items.map((item, index) => (
+          <div key={item.name} className="py-4">
+            <div className="flex gap-x-4 ">
+              <div className="flex-1">{item.name}</div>
+              <div className="flex-1">{item.price}</div>
 
-            <div className="flex gap-x-2">
-              <span>{item.quantity}</span>
-              <div>
-                <button
-                  onClick={() => updateQuantity(index, -1)}
-                  className="bg-red-500 text-white p-1 rounded"
-                >
-                  -
-                </button>
-                <button
-                  onClick={() => updateQuantity(index, 1)}
-                  className="bg-green-500 text-white p-1 rounded"
-                >
-                  +
-                </button>
+              <div className="flex gap-x-2">
+                <span>{item.quantity}</span>
+                <div>
+                  <button
+                    onClick={() => updateQuantity(index, -1)}
+                    className="bg-red-500 text-white py-1 px-3 rounded-full rounded"
+                  >
+                    -
+                  </button>
+                  <button
+                    onClick={() => updateQuantity(index, 1)}
+                    className="bg-green-500 text-white py-1 px-3 rounded-full rounded"
+                  >
+                    +
+                  </button>
+                </div>
+              </div>
+              <div className="flex-1 text-right">
+                <FormattedPrice value={item.price * item.quantity} />
               </div>
             </div>
-            <div className="flex-1 text-right">
-              <FormattedPrice value={item.price * item.quantity} />
-            </div>
           </div>
-        </div>
-      ))}
+        ))}
 
-      {/* TOTAL Line */}
-      <div className="flex flex-1 justify-between font-bold mt-4">
-        <div className="flex-1">TOTAL</div>
-        <div className="flex-1 text-right">
-          <FormattedPrice value={totalPrice} />
+        {/* TOTAL Line */}
+        <div className="flex flex-1 justify-between font-bold py-4">
+          <div className="flex-1">TOTAL</div>
+          <div className="flex-1 text-right">
+            <FormattedPrice value={totalPrice} />
+          </div>
         </div>
       </div>
     </>
