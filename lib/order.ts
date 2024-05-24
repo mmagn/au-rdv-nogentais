c8d52b9492a13d05f87a1660f924a98e687ce155
Mathieu Magnin <mathieu.magnin@gmail.com>
2024-05-24 10:40:23 +0200

    timezone

--- lib/order.ts
+++ lib/order.ts
@@ -1,5 +1,12 @@
 import dayjs from "dayjs";
 import "dayjs/locale/fr";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+const TIMEZONE = "Europe/Paris";
 
 export const total = (items: { quantity: number; price: number }[]) => {
   return items.reduce((acc, item) => acc + item.quantity * item.price, 0);
@@ -14,8 +21,8 @@ export const paymentMethodHumanized = (method: string) => {
 };
 
 export const dateHumanized = (date: Date) => {
-  return dayjs(date).locale("fr").format("DD/MM/YYYY");
+  return dayjs(date).locale("fr").tz(TIMEZONE).format("DD/MM/YYYY");
 };
 export const hoursHumanized = (date: Date) => {
-  return dayjs(date).locale("fr").format("HH:mm");
+  return dayjs(date).locale("fr").tz(TIMEZONE).format("HH:mm");
 };
