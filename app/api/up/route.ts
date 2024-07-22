9f0eed610fb3ae49910c6abf8057442f39afea0f
Mathieu Magnin <mathieu.magnin@gmail.com>
2024-07-22 19:28:26 +0200

    force revalidate

--- app/api/up/route.ts
+++ app/api/up/route.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import { localDayjs } from "@/lib/dayjs";
 
 export const fetchCache = "force-no-store";
+export const revalidate = 3600;
 
 export async function GET(request: Request) {
   const prisma = new PrismaClient();
